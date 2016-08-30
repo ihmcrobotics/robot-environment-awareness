3748d05916552edf39213ee450b5302a261048d6
Sylvain Bertrand <sbertrand@ihmc.us>
2016-08-30 12:08:57 -0500

    Added DepthDataStateCommand to the net class list.

--- src/us/ihmc/robotEnvironmentAwareness/communication/LidarSimulationNetClassList.java
+++ src/us/ihmc/robotEnvironmentAwareness/communication/LidarSimulationNetClassList.java
@@ -4,6 +4,7 @@ import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
 
 import us.ihmc.communication.net.NetClassList;
+import us.ihmc.humanoidRobotics.communication.packets.sensing.DepthDataStateCommand;
 import us.ihmc.humanoidRobotics.communication.packets.sensing.PointCloudWorldPacket;
 import us.ihmc.robotics.lidar.LidarScanParameters;
 
@@ -13,6 +14,7 @@ public class LidarSimulationNetClassList extends NetClassList
    {
       registerPacketClass(LidarPosePacket.class);
       registerPacketClass(PointCloudWorldPacket.class);
+      registerPacketClass(DepthDataStateCommand.class);
 
       registerPacketField(float[].class);
       registerPacketField(Quat4d.class);

88fca4571a066bf9b1ce111f54f9943a3541e2cd
Sylvain Bertrand <sbertrand@ihmc.us>
2016-11-27 13:14:48 -0600

    Cleaned up and improved PointMean and VectorMean.

--- src/us/ihmc/robotEnvironmentAwareness/geometry/PointMean.java
+++ src/us/ihmc/robotEnvironmentAwareness/geometry/PointMean.java
@@ -1,46 +1,50 @@
 package us.ihmc.robotEnvironmentAwareness.geometry;
 
 import javax.vecmath.Point3d;
-
-import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import javax.vecmath.Tuple3d;
 
 public class PointMean extends Point3d
 {
    private static final long serialVersionUID = -3110940850302600107L;
 
-   private final Mean meanX = new Mean();
-   private final Mean meanY = new Mean();
-   private final Mean meanZ = new Mean();
+   private int sampleSize = 0;
 
    public PointMean()
    {
    }
 
-   public void update(Point3d point)
+   public void update(Tuple3d tuple)
    {
-      update(point.getX(), point.getY(), point.getZ());
+      update(tuple.getX(), tuple.getY(), tuple.getZ());
+   }
+
+   public void update(Tuple3d tuple, int updateSize)
+   {
+      update(tuple.getX(), tuple.getY(), tuple.getZ(), updateSize);
    }
 
    public void update(double x, double y, double z)
    {
-      meanX.increment(x);
-      meanY.increment(y);
-      meanZ.increment(z);
-      setX(meanX.getResult());
-      setY(meanY.getResult());
-      setZ(meanZ.getResult());
+      update(x, y, z, 1);
+   }
+
+   public void update(double x, double y, double z, int updateSize)
+   {
+      sampleSize += updateSize;
+      double nInv = (double) updateSize / (double) sampleSize;
+      this.x += (x - this.x) * nInv;
+      this.y += (y - this.y) * nInv;
+      this.z += (z - this.z) * nInv;
    }
 
    public void clear()
    {
-      meanX.clear();
-      meanY.clear();
-      meanZ.clear();
+      sampleSize = 0;
       set(0.0, 0.0, 0.0);
    }
 
    public int getNumberOfSamples()
    {
-      return (int) meanX.getN();
+      return sampleSize;
    }
 }

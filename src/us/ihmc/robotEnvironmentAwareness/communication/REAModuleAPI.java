963d7112a49a9b6b1d040547d785d4647cd5e271
Sylvain Bertrand <sbertrand@ihmc.us>
2016-12-06 19:09:47 -0600

    Added the option to clear the polygons.

--- src/us/ihmc/robotEnvironmentAwareness/communication/REAModuleAPI.java
+++ src/us/ihmc/robotEnvironmentAwareness/communication/REAModuleAPI.java
@@ -79,6 +79,7 @@ public class REAModuleAPI
    public static final Topic<Boolean> PlanarRegionsSegmentationClear = PlanarRegionsCategory.child(Segmentation).topic(Clear);
    public static final Topic<PlanarRegionSegmentationParameters> PlanarRegionsSegmentationParameters = PlanarRegionsCategory.child(Segmentation).topic(Parameters);
    public static final Topic<Boolean> PlanarRegionsPolygonizerEnable = PlanarRegionsCategory.child(Polygonizer).topic(Enable);
+   public static final Topic<Boolean> PlanarRegionsPolygonizerClear = PlanarRegionsCategory.child(Polygonizer).topic(Clear);
    public static final Topic<PolygonizerParameters> PlanarRegionsPolygonizerParameters = PlanarRegionsCategory.child(Polygonizer).topic(Parameters);
    public static final Topic<Boolean> PlanarRegionsIntersectionEnable = PlanarRegionsCategory.child(Intersection).topic(Enable);
    public static final Topic<IntersectionEstimationParameters> PlanarRegionsIntersectionParameters = PlanarRegionsCategory.child(Intersection).topic(Parameters);

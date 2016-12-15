b660f8fc4a34576e0a8ceb21d911e40ef68747a3
Sylvain Bertrand <sbertrand@ihmc.us>
2016-12-14 21:17:34 -0600

    Switched the data type used in the data importer and visualizers.

--- src/us/ihmc/robotEnvironmentAwareness/planarRegion/PlanarRegionSegmentationRawData.java
+++ src/us/ihmc/robotEnvironmentAwareness/planarRegion/PlanarRegionSegmentationRawData.java
@@ -1,13 +1,17 @@
 package us.ihmc.robotEnvironmentAwareness.planarRegion;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.vecmath.Point2d;
 import javax.vecmath.Point3d;
+import javax.vecmath.Point3f;
 import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
+import javax.vecmath.Vector3f;
 
 import us.ihmc.jOctoMap.node.NormalOcTreeNode;
 import us.ihmc.robotEnvironmentAwareness.communication.packets.PlanarRegionSegmentationMessage;
@@ -20,6 +24,42 @@ public class PlanarRegionSegmentationRawData
    private final Point3d origin;
    private final List<Point3d> pointCloud;
    private final Quat4d orientation;
+
+   public PlanarRegionSegmentationRawData(int regionId, Vector3f normal, Point3f origin)
+   {
+      this.regionId = regionId;
+      this.normal = new Vector3d(normal);
+      this.origin = new Point3d(origin);
+      pointCloud = new ArrayList<>();
+      orientation = PolygonizerTools.getRotationBasedOnNormal(normal);
+   }
+
+   public PlanarRegionSegmentationRawData(int regionId, Vector3d normal, Point3d origin)
+   {
+      this.regionId = regionId;
+      this.normal = new Vector3d(normal);
+      this.origin = new Point3d(origin);
+      pointCloud = new ArrayList<>();
+      orientation = PolygonizerTools.getRotationBasedOnNormal(normal);
+   }
+
+   public PlanarRegionSegmentationRawData(int regionId, Vector3f normal, Point3f origin, List<Point3f> pointCloud)
+   {
+      this.regionId = regionId;
+      this.normal = new Vector3d(normal);
+      this.origin = new Point3d(origin);
+      this.pointCloud = pointCloud.stream().map(point3f -> new Point3d(point3f)).collect(Collectors.toList());
+      orientation = PolygonizerTools.getRotationBasedOnNormal(normal);
+   }
+
+   public PlanarRegionSegmentationRawData(int regionId, Vector3d normal, Point3d origin, List<Point3d> pointCloud)
+   {
+      this.regionId = regionId;
+      this.normal = new Vector3d(normal);
+      this.origin = new Point3d(origin);
+      this.pointCloud = pointCloud.stream().map(point -> new Point3d(point)).collect(Collectors.toList());
+      orientation = PolygonizerTools.getRotationBasedOnNormal(normal);
+   }
 
    public PlanarRegionSegmentationRawData(PlanarRegionSegmentationNodeData ocTreeNodePlanarRegion)
    {
@@ -88,6 +128,16 @@ public class PlanarRegionSegmentationRawData
    public Quat4d getOrientation()
    {
       return orientation;
+   }
+
+   public Stream<Point3d> stream()
+   {
+      return pointCloud.stream();
+   }
+
+   public Stream<Point3d> parallelStream()
+   {
+      return pointCloud.parallelStream();
    }
 
    public RigidBodyTransform getTransformFromLocalToWorld()
